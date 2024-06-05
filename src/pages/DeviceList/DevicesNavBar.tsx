b801691d946e390bde25b2b6b95ae39ec092aebb
Artur K <tornraxx@gmail.com>
2024-06-06 01:43:58 +0700

    feat(devices navbar): add data skeleton setter in fetch func

--- src/pages/DeviceList/DevicesNavBar.tsx
+++ src/pages/DeviceList/DevicesNavBar.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
+import { AppBar, Box, Toolbar, Container, Button } from '@mui/material/';
 import SearchField from './SearchField';
 import { getAllDevices } from '../../services/apiData';
 import { useContext, useEffect } from 'react';
@@ -13,18 +9,21 @@ import AlertModal from '../../components/modal/AlertModal';
 
 export default function DevicesNavBar() {
 
-    const { email, password, devicesData, setDevicesData, toggleShowModal, setErrorMsg } = useContext(DataContext);
+    const { email, password, devicesData, setDevicesData, toggleShowModal, setErrorMsg, setLoading } = useContext(DataContext);
 
     useEffect(() => {
     }, [devicesData])
 
     const fetchAllDevicesData = async () => {
         try {
+            setLoading(true);
             const response = await getAllDevices(email, password);
             if (response.length === 0) {
+                setLoading(false);
                 setErrorMsg('Не получилось загрузить данные. Попробуйте позже');
                 toggleShowModal(true);
             } else {
+                setLoading(false);
                 setDevicesData(response);
             }
         }
@@ -44,10 +43,15 @@ export default function DevicesNavBar() {
                 <AlertModal />
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex', md: 'flex', lg: 'flex', xl: 'flex' } }}>
+                        <Box sx={{ flexGrow: 1, display: 'flex' }}>
                             <Button
                                 onClick={handleDeviceList}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
+                                sx={{
+                                    my: 2, display: 'block', color: '#ffffff',
+                                    "&:hover": {
+                                        textDecoration: 'underline #ffffff'
+                                    }
+                                }}
                             >Show all</Button>
                         </Box>
                         <SearchField />
