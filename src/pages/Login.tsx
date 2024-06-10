75af7fcbc7413d851c77c662904a81060e579f6c
Artur K <tornraxx@gmail.com>
2024-06-10 15:55:09 +0700

    feat(login page): add react hook validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,17 @@
-import * as React from 'react';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { Typography, Container, Box, Grid, Link, Checkbox, FormControlLabel, TextField, CssBaseline, Button, Avatar } from '@mui/material/';
-import { useState, useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import DataContext from '../context/DataProvider';
 import { useNavigate } from 'react-router-dom'
 import { setAuthSession } from '../services/apiData';
 import AlertModal from '../components/modal/AlertModal';
+import { useForm, Controller } from 'react-hook-form';
+
+
+interface IFormInputs {
+    email: string;
+    password: string;
+}
 
 
 function Copyright() {
@@ -24,21 +30,29 @@ function Copyright() {
 
 export default function Login() {
 
-    const { authStatus, email, password, setAuthStatus, toggleShowModal, setErrorMsg, setEmail, setPassword } = useContext(DataContext);
-    const [errorMail, setMailError] = useState(false);
-    const [errorPwd, setPwdError] = useState(false);
+    const { formValues: { email, password }, setErrorMsg, toggleShowModal, setAuthStatus, setFormValues } = useContext(DataContext);
+
+    useEffect(() => {
+        if (password) {
+            fetchSession()
+        }
+    }, [password])
+
     const navigate = useNavigate();
 
-    useEffect(() => {
+    const { handleSubmit, control, formState: { isValid, isDirty } } = useForm<IFormInputs>({
+        mode: "all"
+    });
 
-    }, [])
-
-    useEffect(() => {
-    }, [authStatus, email, password])
+    const formSubmit = (formData: IFormInputs, e?: React.BaseSyntheticEvent) => {
+        e?.preventDefault();
+        setFormValues(formData);
+    }
 
     const fetchSession = async () => {
         try {
             const response = await setAuthSession(email, password);
+            console.log(response);
             if (response?.status === 200) {
                 setAuthStatus(true);
                 navigate('/');
@@ -56,29 +70,8 @@ export default function Login() {
         }
     }
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        await fetchSession()
-    };
-
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value)
-        if (e.target.validity.valid) {
-            setMailError(false);
-        } else {
-            setMailError(true);
-        }
-    };
-
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
-        if (e.target.validity.valid) {
-            setPwdError(false);
-        } else {
-            setPwdError(true);
-        }
-    };
-
+    const emailPattern = /^(?=.{1,256})(?=.{1,64}@.{0,63}.{1,255}$)(((?:[A-Za-z0-9_%!#$&'*+/=?^`{|}~-]+\.)*[A-Za-z0-9_%!#$&'*+/=?^`{|}~-]){1,64})+@(?:[A-Za-z0-9]+(\.|-))*(?:[A-Za-z0-9]+(\.|-))*[A-Za-z]{1,63}$/;
+    const passwordPattern = /[A-Za-z0-9/~`! @#$%^&*()_+={[}:;"'<,>.?-]+$/;
 
     return (
         <Container component="main" maxWidth="xs">
@@ -98,34 +91,85 @@ export default function Login() {
                 <Typography component="h1" variant="h5">
                     Sign in
                 </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
+                <Box component="form" onSubmit={handleSubmit(formSubmit)} noValidate sx={{ mt: 1 }}>
+                    <Controller
                         name="email"
-                        autoComplete="email"
-                        autoFocus
-                        value={email}
-                        error={errorMail}
-                        helperText={"Заполните пустое поле"}
-                        onChange={handleEmailChange}
+                        control={control}
+                        defaultValue=""
+                        render={({ field, fieldState: { error } }) => (
+                            <TextField
+                                {...field}
+                                error={!!error}
+                                margin="normal"
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                autoComplete="email"
+                                helperText={error ? error.message : ''}
+                            // value={formValues.email}
+                            // onChange={(e) => setValues((prevState) => {
+                            //     return ({
+                            //         ...prevState,
+                            //         email: e.target.value
+                            //     });
+                            // })}
+                            />
+                        )}
+                        rules={{
+                            required: "Поле Email обязательно к заполнению",
+                            pattern: {
+                                value: emailPattern,
+                                message: "Недопустимый Email. Проверьте пожалуйста введенные данные",
+                            },
+                            minLength: {
+                                value: 3,
+                                message: "Email должен состоять минимум из 3 символов",
+                            },
+                            maxLength: {
+                                value: 256,
+                                message: "Email превышает максимальный лимит в 256 символов",
+                            },
+                        }}
                     />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
+                    <Controller
                         name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value={password}
-                        error={errorPwd}
-                        helperText={"Заполните пустое поле"}
-                        onChange={handlePasswordChange}
+                        control={control}
+                        defaultValue=""
+                        render={({ field, fieldState: { error } }) => (
+                            <TextField
+                                {...field}
+                                error={!!error}
+                                margin="normal"
+                                fullWidth
+                                id="password"
+                                type="password"
+                                label="Password"
+                                autoComplete="current-password"
+                                helperText={error ? error?.message : ''}
+                            // value={formValues.password}
+                            // onChange={(e) => setValues((prevState) => {
+                            //     return ({
+                            //         ...prevState,
+                            //         password: e.target.value
+                            //     });
+                            // })}
+                            />
+                        )}
+                        rules={{
+                            required: "Поле Password обязательно к заполнению",
+                            pattern: {
+                                value: passwordPattern,
+                                message: "Введен недопустимый символ. Проверьте пожалуйста данные",
+                            },
+                            minLength: {
+                                value: 6,
+                                message: "Пароль должен состоять минимум из 6 символов",
+                            },
+                            maxLength: {
+                                value: 100,
+                                message: "Пароль превышает максимальный лимит в 100 символов",
+                            },
+                        }}
                     />
                     <FormControlLabel
                         control={<Checkbox value="remember" color="primary" />}
@@ -135,6 +179,7 @@ export default function Login() {
                         type="submit"
                         fullWidth
                         variant="contained"
+                        disabled={!isDirty || !isValid}
                         sx={{ mt: 3, mb: 2, color: '#ffffff', backgroundColor: '#4b44f4' }}
                     >Sign In
                     </Button>
